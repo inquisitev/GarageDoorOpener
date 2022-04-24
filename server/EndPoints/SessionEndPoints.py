c303b79bf1c0dd110d59c3e7f37503df2dfd89f6
Trevor Keegan <positev@outlook.com>
2022-04-23 23:43:57 -0500

    add session end point test

--- server/EndPoints/SessionEndPoints.py
+++ server/EndPoints/SessionEndPoints.py
@@ -2,11 +2,11 @@ import json
 from flask import request
 
 def make_end_points(app, session_manager):
-    @app.route('/login')
-    def login():
-        data = request.data
+    @app.route('/login', methods=['POST'])
+    def login(): 
+        data = json.loads(request.data)
         token = session_manager.new_user(data['user'], data['email'], data['password_plain'])
-        return json.dumps({'token': token})
+        return json.dumps({'token': token}), 200
 
     @app.route('/signup')
     def signup():
--- server/Tests/test_SessionEndPoints.py
+++ server/Tests/test_SessionEndPoints.py
@@ -1,13 +1,18 @@
 from server.app.app import make_app
-import json 
+import json, os
 
-def test_new_user_endpoint():
-  app = make_app('./tempdb.json')
-  response = app.post('/login', data={
+def test_new_user_endpoint_good():
+
+  if os.path.exists('./tempdb.json'):
+      os.remove('./tempdb.json')
+  app = make_app('./tempdb.json').test_client()
+  res = app.post('/login', data=json.dumps({
     'user': 'test',
     'email': 'test@test.com',
     'password_plain': 'testpassword12345678',
-    }, 
-    content_type='application/json',)
-  assert 'token' in json.loads(response.get_data())
+    }), 
+    content_type='application/json')
+  js = json.loads(res.data)
+  assert res.status_code == 200
+  assert 'token' in js
   
