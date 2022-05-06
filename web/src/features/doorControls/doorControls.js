b4ed9b6571af3687bdb7aebb6f2875108e2b90b2
Trevor Keegan <44409350+Positev@users.noreply.github.com>
2022-05-05 20:46:30 -0500

    Update doorControls.js

--- web/src/features/doorControls/doorControls.js
+++ web/src/features/doorControls/doorControls.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import DoorControlsComponent from './doorControlsComponent';
-import { triggerDoor } from './doorControlSlice';
+import { selectCurrentDoorState, triggerDoor } from './doorControlSlice';
 
 export const DoorControls = () => {
     const dispatch = useDispatch()
-    const doorState = useSelector( door)
+    const doorState = useSelector(selectCurrentDoorState)
 
      return <DoorControlsComponent doorTrigger={()=>{dispatch(triggerDoor())}}/>
   }
