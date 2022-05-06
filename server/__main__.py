ca6278b221b0829c61e060b2be2e301d032b12d7
Trevor Keegan <keegant@uwplatt.edu>
2022-05-05 21:18:09 -0500

    replace main.py for tests

--- server/__main__.py
+++ server/__main__.py
@@ -1,2 +1,14 @@
 
   
+from server.app.app import make_app
+import os
+
+if os.name == 'posix':
+  from server.GarageDoorAdapters.pi.PiFaceDoorAdapter import PiFaceDoorAdapter
+  app,cors = make_app('./data/db.json', PiFaceDoorAdapter())
+else:
+  from server.GarageDoorAdapters.GenericGarageDoorAdapter import GenericGarageDoorAdapter
+  app,cors = make_app('./data/db.json', GenericGarageDoorAdapter())
+  
+
+app.run('0.0.0.0', 3000)
