d452643bd49faa04b4f5c083fca6749ba9d2c2c6
Trevor Keegan <44409350+Positev@users.noreply.github.com>
2022-05-07 07:38:25 -0500

    16 dockerize and nginx (#18)
    * Add dockerfile for web + nginx
    * Add dockerfile for api + gunicorn
    * Add docker-compose
    * Route api through nginx
    * Add "docker compose must succeed" action

---
+++ api/server/__init__.py
@@ -0,0 +1,17 @@
+from shelve import DbfilenameShelf
+from server.app.app import make_app
+import os
+
+DB_FOLDER = './data'
+DB_PATH = f'{DB_FOLDER}/db.json'
+
+if not os.path.exists(DB_FOLDER):
+  os.mkdir(DB_FOLDER)
+
+if os.name == 'posix':
+  from server.GarageDoorAdapters.pi.PiFaceDoorAdapter import PiFaceDoorAdapter
+  api,cors = make_app(DB_PATH, PiFaceDoorAdapter())
+else:
+  from server.GarageDoorAdapters.GenericGarageDoorAdapter import GenericGarageDoorAdapter
+  api,cors = make_app(DB_PATH, GenericGarageDoorAdapter())
+    
