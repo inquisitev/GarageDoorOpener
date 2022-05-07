e9424420ae5de8861734883af6ac12d447b28b15
Trevor Keegan <positev@outlook.com>
2022-05-06 23:24:44 -0500

    make db folder if not there

--- api/server/__init__.py
+++ api/server/__init__.py
@@ -1,13 +1,17 @@
+from shelve import DbfilenameShelf
 from server.app.app import make_app
 import os
 
-DB_PATH = './data/db.json'
+DB_FOLDER = './data/db.json'
+DB_PATH = f'{DB_FOLDER}/db.json'
 
-if os.path.exists(DB_PATH):
-  if os.name == 'posix':
-    from server.GarageDoorAdapters.pi.PiFaceDoorAdapter import PiFaceDoorAdapter
-    api,cors = make_app(DB_PATH, PiFaceDoorAdapter())
-  else:
-    from server.GarageDoorAdapters.GenericGarageDoorAdapter import GenericGarageDoorAdapter
-    api,cors = make_app(DB_PATH, GenericGarageDoorAdapter())
+if not os.path.exists(DB_FOLDER):
+  os.mkdir(DB_FOLDER)
+  
+if os.name == 'posix':
+  from server.GarageDoorAdapters.pi.PiFaceDoorAdapter import PiFaceDoorAdapter
+  api,cors = make_app(DB_PATH, PiFaceDoorAdapter())
+else:
+  from server.GarageDoorAdapters.GenericGarageDoorAdapter import GenericGarageDoorAdapter
+  api,cors = make_app(DB_PATH, GenericGarageDoorAdapter())
     
