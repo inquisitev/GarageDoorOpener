af5a0b68b0707734c4ae6d9e5e1bdb50862aafce
Trevor Keegan <positev@outlook.com>
2022-05-07 06:31:57 -0500

    Update __init__.py

--- api/server/__init__.py
+++ api/server/__init__.py
@@ -2,12 +2,12 @@ from shelve import DbfilenameShelf
 from server.app.app import make_app
 import os
 
-DB_FOLDER = './data/db.json'
+DB_FOLDER = './data'
 DB_PATH = f'{DB_FOLDER}/db.json'
 
 if not os.path.exists(DB_FOLDER):
   os.mkdir(DB_FOLDER)
-  
+
 if os.name == 'posix':
   from server.GarageDoorAdapters.pi.PiFaceDoorAdapter import PiFaceDoorAdapter
   api,cors = make_app(DB_PATH, PiFaceDoorAdapter())
