8ecd7c89a657be7d141c98a3fc0aa0c44623557c
Trevor Keegan <positev@outlook.com>
2022-05-08 09:05:17 -0500

    add apiflask

--- api/server/app/app.py
+++ api/server/app/app.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import json
-from flask import Flask
+from apiflask import APIFlask, Schema, abort
+from apiflask.fields import Integer, String
+from apiflask.validators import Length, OneOf
 from flask_cors import CORS
 from server.EndPoints.SessionEndPoints import make_end_points as make_session_end_points
 from server.EndPoints.GarageDoorEndpoints import make_end_points as make_door_end_points
 from server.Controllers.SessionManager import SessionManager
 
 def make_app(dbpath, opener = None):
-    app = Flask(__name__)
-    cors = CORS(app)
 
+    app = APIFlask(__name__)
+    cors = CORS(app)
     app.config['CORS_HEADERS'] = 'application/json'
-    @app.route('/')
+    @app.get('/')
+    @app.doc(summary='Say hello', description='Some description for the /hello')
     def index():
         return json.dumps({'name': 'alice',
                         'email': 'alice@outlook.com'})
