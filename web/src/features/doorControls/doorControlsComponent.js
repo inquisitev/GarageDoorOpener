3d9388847ef0ae98c1f43d202e93586574d24d3c
Trevor Keegan <positev@outlook.com>
2022-05-24 07:32:24 -0500

    add door state button

--- web/src/features/doorControls/doorControlsComponent.js
+++ web/src/features/doorControls/doorControlsComponent.js
@@ -3,6 +3,7 @@ import { Button } from "antd"
 const DoorControlsComponent = (props) => {
     return <div>
         <p>DoorState: {props.doorState}</p>
+        <button onClick={props.getDoorState}>Update Door State</button>
         <button onClick={props.doorTrigger}>Trigger Door</button>
     </div>
 }
